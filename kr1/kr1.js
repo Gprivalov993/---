8c6a5037bbce4a4d8d70ceba4a122769893c3aef
Gprivalov993 <91178574+Gprivalov993@users.noreply.github.com>
2021-09-22 09:49:16 +0300

    Add files via upload

---
+++ kr1/kr1.js
@@ -0,0 +1,11 @@
+document.writeln("Объявите переменную и запишите в нее свое имя как литерал строки.");
+let name = "Георгий"; 
+document.writeln("Мое имя: " + name);
+
+document.writeln("Объявите константы и запишите в нее месяц и год своего рождения как литерал числа.");
+let mounth = 11; 
+let year = 2001; 
+document.writeln("Месяц и год моего рождения: " + mounth + "." + year);
+
+document.writeln("Создайте функцию, которая печатает приветствие и имеет три  аргумента: name, month, year.");
+document.writeln("Привет, " + name + ". " + "Твой год и месяц рождения: " + year + "." + mounth);
