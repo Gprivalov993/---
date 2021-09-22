e31f57071a4a9ef866bff9a41511eb62a793aee0
Gprivalov993 <91178574+Gprivalov993@users.noreply.github.com>
2021-09-22 10:10:44 +0300

    Add files via upload

--- kr1/kr1.js
+++ kr1/kr1.js
@@ -1,11 +1,17 @@
-document.writeln("Объявите переменную и запишите в нее свое имя как литерал строки.");
+document.writeln("1.Объявите переменную и запишите в нее свое имя как литерал строки.");
 let name = "Георгий"; 
-document.writeln("Мое имя: " + name);
+document.writeln("</br>Мое имя: " + name + "</br>");
 
-document.writeln("Объявите константы и запишите в нее месяц и год своего рождения как литерал числа.");
+document.writeln("</br>2.Объявите константы и запишите в нее месяц и год своего рождения как литерал числа.");
 let mounth = 11; 
 let year = 2001; 
-document.writeln("Месяц и год моего рождения: " + mounth + "." + year);
+document.writeln("</br>Месяц и год моего рождения: " + mounth + "." + year + "</br>");
 
-document.writeln("Создайте функцию, которая печатает приветствие и имеет три  аргумента: name, month, year.");
+document.writeln("</br>3.Создайте функцию, которая печатает приветствие и имеет три  аргумента: name, month, year.");
+document.writeln("</br>Привет, " + name + ". " + "Твой год и месяц рождения: " + year + "." + mounth + "</br>");
+
+document.writeln("</br>4.Реализуйте функцию range(start: number, end: number): array которая отдает массив чисел из диапазона [-15, 15] включая крайние числа.");
+
+
+
 document.writeln("Привет, " + name + ". " + "Твой год и месяц рождения: " + year + "." + mounth);
